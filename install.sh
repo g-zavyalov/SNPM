6f2a7260206aa0ef9135031ecaf9506e1bfad9c5
Gleb Zavyalov <56771324+zavygy@users.noreply.github.com>
2022-01-11 18:40:06 +0300

    install.sh : )

--- install.sh
+++ install.sh
@@ -5,7 +5,7 @@ cd .build/release
 cp -f snpm /usr/local/bin/snpm
 
 mkdir -p ~/.zsh/completion
-touch -p ~/.zsh/completion/_snpm.zsh
+touch ~/.zsh/completion/_snpm.zsh
 snpm --generate-completion-script zsh > ~/.zsh/completion/_snpm.zsh
 echo 'fpath=(~/.zsh/completion ${fpath})' >> ~/.zshrc
 echo 'autoload -U compinit && compinit' >> ~/.zshrc
